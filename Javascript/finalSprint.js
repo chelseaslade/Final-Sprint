db2e9ad52888997eb153bc30d4021351c17141e3
Chelsea Slade (Mayne) <153530277+chelseaslade@users.noreply.github.com>
2024-04-07 13:23:49 -0230

    Add files via upload

---
+++ Javascript/finalSprint.js
@@ -0,0 +1,54 @@
+//Finail Sprint: Javascript
+//Author: Chelsea Slade (Mayne)
+//Date: April 3, 2024
+
+//Functions
+function printFullName(patient) {
+  return `${patient.fName} ${patient.lName}`;
+}
+
+function printfamDoc(patient) {
+  return `${patient.familyDoc}`;
+}
+
+function contactInformation(patient) {
+  return `Email: ${patient.phoneNum} Phone Number: ${patient.email}`;
+}
+
+//Fetch JSON Data
+fetch("patients.json")
+  .then((myObject) => myObject.json())
+  .then((data) => {
+    //Container for Patient Data
+    const container = document.createElement("div");
+    container.id = "patientContainer";
+
+    //Loop through patients in JSON
+    data.forEach((patient) => {
+      const patientDiv = document.createElement("div");
+      patientDiv.className = "patient";
+
+      //Add data to div
+      patientDiv.innerHTML = `
+        <h3>${printFullName(patient)}</h3>
+        <p>Family Doctor: ${printfamDoc(patient)}</br>
+        ${contactInformation(patient)}</p>
+        `;
+
+      //Add div to container
+      container.appendChild(patientDiv);
+
+      //Print functions to console
+      console.log(printFullName(patient));
+      console.log(printfamDoc(patient));
+      console.log(contactInformation(patient));
+    });
+
+    //Add container to HTML
+    document.body.appendChild(container);
+  })
+
+  //Error Handling
+  .catch((error) => {
+    console.error(error);
+  });
